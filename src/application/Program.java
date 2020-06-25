5f01a98896a41752d2913ceafbaa387f59f17036
br-sg <brunosog@gmail.com>
2020-06-25 15:25:22 -0300

    Final

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,18 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine();
+					while (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
+						System.out.print("Invalid value");
+						type = sc.nextLine();
+
+					}
+					chessMatch.replacePromotedPiece(type);
+
+				}
 
 			} catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
